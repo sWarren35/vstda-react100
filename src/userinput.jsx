5f7c083e73b47acea3cf5520b20b3bdc585158f4
Samuel <smlsdog18@yahoo.com>
2019-01-18 14:08:30 -0800

    code review √

--- src/userinput.jsx
+++ src/userinput.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 const Userinput = props => {
   return(
-      <div className="panel panel-default">
-        <div className="panel-heading">Add New Todo</div>
-        <div className="panel-body">
-          <textarea 
-            className="create-todo-text"
-            value={props.value} 
-            onChange={props.onChange}>
-          </textarea>
-          <div className="form-group">
-            <label className="label-move">&nbsp;Priority:&nbsp;</label>
-            <select className="create-todo-priority form-control" style={{'width':'100%'}} onChange={props.changePriority}>
-              <option value="1">Low</option>
-              <option value="2">Medium</option>
-              <option value="3">High</option>
-            </select> 
-          </div>
-        </div>
-        <div className="panel-footer" style={{'width':'100%'}}>
-          <button className="btn-gradient green block create-todo move-btn" onClick={props.onSubmit}>&nbsp;➕</button>
+    <div className="panel panel-default">
+      <div className="panel-heading">Add New Todo</div>
+      <div className="panel-body">
+        <textarea 
+          className="create-todo-text"
+          value={props.value} 
+          onChange={props.onChange}>
+        </textarea>
+        <div className="form-group">
+          <label className="label-move">&nbsp;Priority:&nbsp;</label>
+          <select className="create-todo-priority form-control" style={{'width':'100%'}} onChange={props.changePriority}>
+            <option value="1">Low</option>
+            <option value="2">Medium</option>
+            <option value="3">High</option>
+          </select> 
         </div>
       </div>
+      <div className="panel-footer" style={{'width':'100%'}}>
+        <button className="btn-gradient green block create-todo move-btn" onClick={props.onSubmit}>&nbsp;➕</button>
+      </div>
+    </div>
   )  
 }
 
